84b48be76e9fd5964d86ac3b789fd80899605943
eriknugzarovic <09Erik07@gmail.com>
2024-03-18 20:21:04 +0300

    feat(main): init bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
-import 'package:flutter/material.dart';
-import 'package:wildtodo/app.dart';
-import 'package:wildtodo/core/theme/core/theme_provider.dart';
-import 'package:wildtodo/core/theme/wild_dark_theme.dart';
+import 'dart:async';
 
-void main() {
+import 'package:bloc_concurrency/bloc_concurrency.dart' as bloc_concurrency;
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:wildtodo/app.dart';
+import 'package:wildtodo/common/bloc/bloc_observer.dart';
+import 'package:wildtodo/core/theme/core/theme_provider.dart';
+
+import 'core/theme/wild_dark_theme.dart';
+
+Future<void> main() async {
+  Bloc.observer = AppBlocObserver();
+  Bloc.transformer = bloc_concurrency.sequential<Object?>();
+
   runApp(
     ThemeProvider(
       theme: WildDarkTheme(),
