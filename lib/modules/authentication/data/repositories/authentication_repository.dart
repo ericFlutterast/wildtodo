76dc6481450fb6818ec7c2891bb60ddfde6a6825
eriknugzarovic <09Erik07@gmail.com>
2024-03-20 14:34:09 +0300

    feat(authentication_repository): create repo interface

---
+++ lib/modules/authentication/data/repositories/authentication_repository.dart
@@ -0,0 +1,6 @@
+import 'package:wildtodo/modules/authentication/models/user.dart';
+
+abstract interface class IAuthenticationRepository {
+  Future<void> logout();
+  Future<User> login({required final String login, required final String password});
+}
