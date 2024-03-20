8fa8078d1180c6511fd22992f2cfa7c7ad8dfcbc
eriknugzarovic <09Erik07@gmail.com>
2024-03-20 11:06:25 +0300

    feat(authentication): create state class

--- lib/modules/authentication/bloc/authtorization.dart
+++ lib/modules/authentication/bloc/authtorization.dart
@@ -1,1 +1,12 @@
-sealed class AuthenticationState{}
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+@freezed
+sealed class AuthenticationState extends _$AuthenticationState {
+  const AuthenticationState._();
+
+  factory AuthenticationState.idle() = _IdleAuthenticationState;
+
+  factory AuthenticationState.error() = ErrorAuthenticationState;
+
+  factory AuthenticationState.success() = SuccessAuthenticationState;
+}
