2de41670380f97f458a3f52e33e585320e53a3aa
eriknugzarovic <09Erik07@gmail.com>
2024-03-21 19:14:44 +0300

    feat(authentication_repository): create new method

--- lib/modules/authentication/data/repositories/authentication_repository.dart
+++ lib/modules/authentication/data/repositories/authentication_repository.dart
@@ -3,4 +3,5 @@ import 'package:wildtodo/modules/authentication/models/user.dart';
 abstract interface class IAuthenticationRepository {
   Future<void> logout();
   Future<User> login({required final String login, required final String password});
+  Future<void> createUser({required final String login, required final String password});
 }
