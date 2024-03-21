32cbc251e343acc5999bb0448042da58e03027cd
eriknugzarovic <09Erik07@gmail.com>
2024-03-21 19:31:09 +0300

    feat(authentication_repository): create implementation for repository

--- lib/modules/authentication/data/repositories/authentication_repository.dart
+++ lib/modules/authentication/data/repositories/authentication_repository.dart
@@ -1,7 +1,36 @@
+import 'package:wildtodo/common/dio/network_client.dart';
+import 'package:wildtodo/modules/authentication/data/repositories/authentication_repository_interface.dart';
 import 'package:wildtodo/modules/authentication/models/user.dart';
 
-abstract interface class IAuthenticationRepository {
-  Future<void> logout();
-  Future<User> login({required final String login, required final String password});
-  Future<void> createUser({required final String login, required final String password});
+class AuthenticationRepository implements IAuthenticationRepository {
+  late final NetworkClient _networkClient;
+
+  AuthenticationRepository() {
+    _networkClient = NetworkClient();
+  }
+
+  @override
+  Future<void> createUser({required String email, required String password}) async {
+    _networkClient.request(
+      type: Post(path: '/api/h1/users/', data: {
+        "email": email,
+        "password": password,
+      }),
+    );
+  }
+
+  @override
+  Future<User> login({required String uid, required String email, required String password}) async {
+    final response = await _networkClient.request(
+        type: Post(path: 'api/v1/users/$uid/sessions/', data: {
+      "email": email,
+      "password": password,
+    }));
+
+    //TODO: Распарсить response в юзера
+    return const User.notAuthenticatedUser();
+  }
+
+  @override
+  Future<void> logout() async {}
 }
