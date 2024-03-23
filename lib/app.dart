d5c546e4a2f550dc141ded07bca36451fbcd89ea
eriknugzarovic <09Erik07@gmail.com>
2024-03-23 13:09:07 +0300

    feat(custom_router): make new methods for page transition

--- lib/app.dart
+++ lib/app.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:wildtodo/common/navigation/routs.dart';
+import 'package:go_router/go_router.dart';
 
 class AppRoot extends StatelessWidget {
-  const AppRoot({super.key});
+  final GoRouter router;
+
+  const AppRoot({super.key, required this.router});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Wildtodo',
       theme: ThemeData(),
+      routerConfig: router,
       debugShowCheckedModeBanner: false,
-      routes: routs,
+      builder: (context, child) {
+        return child!;
+      },
     );
   }
 }
