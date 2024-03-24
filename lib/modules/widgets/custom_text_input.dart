f31094a51bbcf87baac7f2134f164fb04ab99a9c
eriknugzarovic <09Erik07@gmail.com>
2024-03-24 14:08:10 +0300

    feat(custom_text_field): create layout for text form field

---
+++ lib/modules/widgets/custom_text_input.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CustomTextInput extends StatefulWidget {
+  const CustomTextInput({super.key});
+
+  @override
+  State<CustomTextInput> createState() => _CustomTextInputState();
+}
+
+class _CustomTextInputState extends State<CustomTextInput> {
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField();
+  }
+}
