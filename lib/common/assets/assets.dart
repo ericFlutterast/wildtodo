2f5cb9f982230f587879262893130afbc832cbfd
eriknugzarovic <09Erik07@gmail.com>
2024-03-25 23:32:55 +0300

    feat(assets): add new path

--- lib/common/assets/assets.dart
+++ lib/common/assets/assets.dart
@@ -3,4 +3,5 @@ final class Assets {
   static const String navbarFriends = 'assets/icons/navbar_button_icons/FriendsIcon.svg';
   static const String navbarNotification = 'assets/icons/navbar_button_icons/NotificationIcon.svg';
   static const String navbarTasks = 'assets/icons/navbar_button_icons/TasksIcon.svg';
+  static const String appLogo = 'assets/icons/app_logo.svg';
 }
