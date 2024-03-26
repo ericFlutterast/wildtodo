8a356a4e1b1765095778383f2f4aa60b4ae24f8f
eriknugzarovic <09Erik07@gmail.com>
2024-03-26 14:46:59 +0300

    feat(wild_app_bar): add new field for fine tuning

--- lib/modules/home/screens/home_screen.dart
+++ lib/modules/home/screens/home_screen.dart
@@ -1,8 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:go_router/go_router.dart';
 import 'package:wildtodo/common/assets/assets.dart';
 import 'package:wildtodo/core/core_utils.dart';
+import 'package:wildtodo/modules/widgets/progress_circular_widget.dart';
 import 'package:wildtodo/modules/widgets/wild_appbar.dart';
 
 class HomeNavigationScreen extends StatefulWidget {
@@ -45,7 +47,28 @@ class _HomeNavigationScreenState extends State<HomeNavigationScreen> {
       child: Scaffold(
         backgroundColor: context.theme.palette.grayscale.g1,
         drawer: const Drawer(),
-        appBar: const WildAppBar(),
+        appBar: WildAppBar(
+          title: Text(
+            'March 2023',
+            style: context.theme.typeface.subheading.bold,
+          ),
+          action: [
+            Icon(
+              CupertinoIcons.bell_fill,
+              color: context.theme.palette.grayscale.g5,
+              size: 16,
+            ),
+            const Padding(
+              padding: EdgeInsets.fromLTRB(16, 7, 24, 7),
+              child: SizedBox(
+                width: 42,
+                child: ProgressCircularWidget(
+                  percent: 0.70,
+                ),
+              ),
+            )
+          ],
+        ),
         body: widget.child,
         bottomNavigationBar: Container(
           height: 80,
