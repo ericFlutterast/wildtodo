066ccb698954f70f58cd4ba865909ac76fe648b4
eriknugzarovic <09Erik07@gmail.com>
2024-03-26 20:12:27 +0300

    feat(registration_screen): validation error handler

--- lib/modules/authentication/ui/screens/registration_screen.dart
+++ lib/modules/authentication/ui/screens/registration_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:reactive_forms/reactive_forms.dart';
 import 'package:wildtodo/core/core_utils.dart';
 import 'package:wildtodo/modules/widgets/custom_text_input.dart';
 import 'package:wildtodo/modules/widgets/signboard.dart';
@@ -14,6 +15,27 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  final _emailFormControl = FormControl<String>(validators: [
+    Validators.required,
+    Validators.email,
+  ]);
+
+  final _passwordFormControl = FormControl<String>(validators: [
+    Validators.required,
+    Validators.minLength(8),
+  ]);
+
+  final _passwordConfirmFormControl = FormControl<String>();
+
+  late final _form = FormGroup(
+    {
+      'email': _emailFormControl,
+      'password': _passwordFormControl,
+      'passwordConfirm': _passwordConfirmFormControl,
+    },
+    validators: [Validators.mustMatch('password', 'passwordConfirm')],
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,33 +54,58 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
           padding: const EdgeInsets.symmetric(horizontal: 16),
           child: Align(
             alignment: Alignment.topCenter,
-            child: Column(
-              children: [
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: MediaQuery.of(context).size.height * 0.08),
-                  child: Signboard(
-                    logo: Icon(
-                      Icons.supervised_user_circle_outlined,
-                      size: 100,
-                      color: context.theme.palette.grayscale.g5,
+            child: ReactiveForm(
+              formGroup: _form,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: MediaQuery.of(context).size.height * 0.08),
+                    child: Signboard(
+                      logo: Icon(
+                        Icons.supervised_user_circle_outlined,
+                        size: 100,
+                        color: context.theme.palette.grayscale.g5,
+                      ),
                     ),
                   ),
-                ),
-                const CustomTextInput(hintText: 'Почта'),
-                const SizedBox(height: 16),
-                const CustomTextInput(hintText: 'Пароль'),
-                const SizedBox(height: 16),
-                const CustomTextInput(hintText: 'Повторить пароль'),
-                const SizedBox(height: 26),
-                UiKitButton.primary(
-                  title: 'Зарегистрироваться',
-                  isSmall: true,
-                  onTap: () {
-                    //TODO:
-                  },
-                ),
-                const SizedBox(height: 16),
-              ],
+                  CustomTextInput(
+                    hintText: 'Почта',
+                    formControl: _emailFormControl,
+                    validationMessage: {
+                      'required': (_) => 'Обязательное поле',
+                      'email': (_) => 'Не корректный формат',
+                    },
+                  ),
+                  const SizedBox(height: 16),
+                  CustomTextInput(
+                    hintText: 'Пароль',
+                    obscureText: true,
+                    formControl: _passwordFormControl,
+                    validationMessage: {
+                      'required': (_) => 'Обязательное поле',
+                      'minLength': (_) => 'Минимальная длина 8 символов'
+                    },
+                  ),
+                  const SizedBox(height: 16),
+                  CustomTextInput(
+                    hintText: 'Повторить пароль',
+                    obscureText: true,
+                    formControl: _passwordConfirmFormControl,
+                    validationMessage: {
+                      'mustMatch': (_) => 'Не соответсвует паролю',
+                    },
+                  ),
+                  const SizedBox(height: 26),
+                  UiKitButton.primary(
+                    title: 'Зарегистрироваться',
+                    isSmall: true,
+                    onTap: () {
+                      //TODO:
+                    },
+                  ),
+                  const SizedBox(height: 16),
+                ],
+              ),
             ),
           ),
         ),
