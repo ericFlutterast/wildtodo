dd389ce6a78156e929f3910a7b9695dd7ccd037c
eriknugzarovic <09Erik07@gmail.com>
2024-03-28 10:42:19 +0300

    refactor(authentication_repository): add slash

--- lib/modules/authentication/data/repositories/authentication_repository.dart
+++ lib/modules/authentication/data/repositories/authentication_repository.dart
@@ -1,4 +1,3 @@
-import 'package:dio/dio.dart';
 import 'package:wildtodo/common/dio/network_client.dart';
 import 'package:wildtodo/modules/authentication/data/repositories/authentication_repository_interface.dart';
 import 'package:wildtodo/modules/authentication/models/user.dart';
@@ -15,7 +14,6 @@ class AuthenticationRepository implements IAuthenticationRepository {
     final response = await _networkClient.request(
       type: Post(
         path: '/api/v1/users/',
-        options: Options(headers: {}),
         data: {
           "email": email,
           "password": password,
@@ -29,10 +27,14 @@ class AuthenticationRepository implements IAuthenticationRepository {
   @override
   Future<User> login({required String uid, required String email, required String password}) async {
     final response = await _networkClient.request(
-        type: Post(path: 'api/v1/users/$uid/sessions/', data: {
-      "email": email,
-      "password": password,
-    }));
+      type: Post(
+        path: '/api/v1/users/$uid/sessions/',
+        data: {
+          "email": email,
+          "password": password,
+        },
+      ),
+    );
 
     //TODO: Распарсить response в юзера
     return const User.notAuthenticatedUser();
