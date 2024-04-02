54b9c6c7769a9c6f01814647f98af3ec019f2c5f
ericFlutterast <erikGitFlutter@gmail.com>
2024-04-02 19:25:39 +0300

    feat(profile_screen): call logout event and add auth state listener

--- lib/modules/profile/ui/screens/profile_screen.dart
+++ lib/modules/profile/ui/screens/profile_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 import 'package:wildtodo/core/core_utils.dart';
+import 'package:wildtodo/modules/authentication/bloc/authentication.dart';
 import 'package:wildtodo/modules/widgets/wild_appbar.dart';
 
 class ProfileScreen extends StatelessWidget {
@@ -7,48 +10,55 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: context.theme.palette.grayscale.g1,
-      appBar: WildAppBar(
-        action: [
-          IconButton(
-            onPressed: () {
-              //TODO: logout
-            },
-            icon: const Icon(Icons.logout),
-          ),
-        ],
-      ),
-      body: SafeArea(
-        child: CustomScrollView(
-          physics: const BouncingScrollPhysics(),
-          slivers: [
-            SliverList(
-              delegate: SliverChildListDelegate(
-                [
-                  CircleAvatar(
-                    child: null,
-                    radius: 50,
-                    backgroundColor: context.theme.palette.grayscale.g5,
-                  ),
-                  const SizedBox(height: 25),
-                  Center(
-                    child: Text(
-                      'User name',
-                      style: context.theme.typeface.body2,
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-                  Center(
-                    child: Text(
-                      'User email',
-                      style: context.theme.typeface.body2,
-                    ),
-                  ),
-                ],
-              ),
+    return BlocListener<AuthenticationBloc, AuthenticationState>(
+      listener: (context, state) {
+        state.mapOrNull(
+          unAuthenticated: (_) => context.pop(),
+        );
+      },
+      child: Scaffold(
+        backgroundColor: context.theme.palette.grayscale.g1,
+        appBar: WildAppBar(
+          action: [
+            IconButton(
+              onPressed: () {
+                context.read<AuthenticationBloc>().add(const AuthenticationEvent.logout());
+              },
+              icon: const Icon(Icons.logout),
             ),
           ],
+        ),
+        body: SafeArea(
+          child: CustomScrollView(
+            physics: const BouncingScrollPhysics(),
+            slivers: [
+              SliverList(
+                delegate: SliverChildListDelegate(
+                  [
+                    CircleAvatar(
+                      child: null,
+                      radius: 50,
+                      backgroundColor: context.theme.palette.grayscale.g5,
+                    ),
+                    const SizedBox(height: 25),
+                    Center(
+                      child: Text(
+                        'User name',
+                        style: context.theme.typeface.body2,
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    Center(
+                      child: Text(
+                        'User email',
+                        style: context.theme.typeface.body2,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
