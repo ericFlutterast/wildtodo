8320fcef214088d24e31a1446eae331b949f9c0c
ericFlutterast <erikGitFlutter@gmail.com>
2024-04-02 13:21:06 +0300

    feat(main): init app dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:wildtodo/app.dart';
 import 'package:wildtodo/common/bloc/bloc_observer.dart';
 import 'package:wildtodo/common/navigation/custom_router.dart';
 import 'package:wildtodo/core/theme/core/theme_provider.dart';
+import 'package:wildtodo/modules/initializer/di/app_dependencies.dart';
 
 import 'core/theme/wild_dark_theme.dart';
 
@@ -14,10 +15,16 @@ Future<void> main() async {
   Bloc.observer = AppBlocObserver();
   Bloc.transformer = bloc_concurrency.sequential<Object?>();
 
+  final dependencies = AppDependencies.instance;
+  dependencies.init();
+
   runApp(
     ThemeProvider(
       theme: WildDarkTheme(),
-      child: AppRoot(router: CustomRouter.router),
+      child: AppRoot(
+        router: CustomRouter.router,
+        dependencies: dependencies,
+      ),
     ),
   );
 }
